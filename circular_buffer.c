68280fd6cb262f9f2edfc464bf1b59d637851c39
Moritz Frisch <mephisto66@posteo.de>
2022-09-10 17:24:42 +0300

    first release

--- circular_buffer.c
+++ circular_buffer.c
@@ -1,1 +1,59 @@
 #include "circular_buffer.h"
+#include <errno.h>
+#include <stdbool.h>
+
+bool empty(circular_buffer_t *buffer) { return buffer->current_size == 0; }
+
+bool full(circular_buffer_t *buffer) {
+  return buffer->current_size == buffer->capacity;
+}
+
+circular_buffer_t *new_circular_buffer(size_t capacity) {
+  circular_buffer_t *buffer = malloc(sizeof(circular_buffer_t));
+  if (!buffer) {
+    errno = ENODATA;
+    exit(EXIT_FAILURE);
+  }
+  buffer->head = 0;
+  buffer->tail = 0;
+  buffer->current_size = 0;
+  buffer->capacity = capacity;
+  buffer->values = malloc(capacity * sizeof(buffer_value_t));
+  if (!buffer->values) {
+    errno = ENODATA;
+    exit(EXIT_FAILURE);
+  }
+  return buffer;
+}
+
+int16_t write(circular_buffer_t *buffer, buffer_value_t value) {
+  if (full(buffer))
+    exit(EXIT_FAILURE);
+  if (buffer->tail == buffer->capacity - 1)
+    buffer->tail = 0;
+  buffer->values[buffer->tail++] = value;
+  buffer->current_size++;
+  return EXIT_SUCCESS;
+}
+
+int16_t overwrite(circular_buffer_t *buffer, buffer_value_t value) {
+  buffer->values[buffer->head] = value;
+  return EXIT_SUCCESS;
+}
+
+int16_t read(circular_buffer_t *buffer, buffer_value_t *value) {
+  if (empty(buffer)) {
+    errno = ENODATA;
+    return EXIT_FAILURE;
+  }
+  *value = buffer->values[buffer->head];
+  buffer->head++;
+  return EXIT_SUCCESS;
+}
+
+void delete_buffer(circular_buffer_t *buffer) { free(buffer); }
+
+void clear_buffer(circular_buffer_t *buffer) {
+  buffer->head = 0;
+  buffer->tail = 1;
+}
--- circular_buffer.h
+++ circular_buffer.h
@@ -7,17 +7,18 @@ typedef uint8_t buffer_value_t;
 
 typedef struct circular_buffer
 {
-    buffer_value_t *buffer;
+  buffer_value_t *values;
+  size_t head;
+  size_t tail;
+  size_t capacity;
+  size_t current_size;
 } circular_buffer_t;
 
 int16_t write(circular_buffer_t *, buffer_value_t);
-
 int16_t overwrite(circular_buffer_t *, buffer_value_t);
-
 int16_t read(circular_buffer_t *, buffer_value_t *);
-
 circular_buffer_t *new_circular_buffer(size_t capacity);
 void delete_buffer(circular_buffer_t *buffer);
 void clear_buffer(circular_buffer_t *buffer);
 
-#endif
+#endif // CIRCULAR_BUFFER_H
