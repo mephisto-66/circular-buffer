eaee7ffa24509a98bd90cf22a46fb0fc26b51181
Moritz Frisch <mephisto66@posteo.de>
2022-09-10 15:21:19 +0300

    header prototypes

---
+++ circular_buffer.h
@@ -0,0 +1,23 @@
+#ifndef CIRCULAR_BUFFER_H
+#define CIRCULAR_BUFFER_H
+
+#include <stdlib.h>
+
+typedef uint8_t buffer_value_t;
+
+typedef struct circular_buffer
+{
+    buffer_value_t *buffer;
+} circular_buffer_t;
+
+int16_t write(circular_buffer_t *, buffer_value_t);
+
+int16_t overwrite(circular_buffer_t *, buffer_value_t);
+
+int16_t read(circular_buffer_t *, buffer_value_t *);
+
+circular_buffer_t *new_circular_buffer(size_t capacity);
+void delete_buffer(circular_buffer_t *buffer);
+void clear_buffer(circular_buffer_t *buffer);
+
+#endif
